1870d5effa4ccc7ae3f6f352426c379eb2ee6131
iiUzila <artisticamente.official@gmail.com>
2025-12-30 22:40:27 -0300

    Only Testing

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Tenta pegar a variável de ambiente. 
+// SE não existir (durante o build), usa uma string falsa para não dar erro.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://lencoyvhrpnwqakxgqbp.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_C7svLkcXDJAeKt0UMscJwg_d82OhiMq";
 
+// Cria o cliente com segurança
 export const supabase = createClient(supabaseUrl, supabaseKey);
